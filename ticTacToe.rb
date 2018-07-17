3a47923ab3d4a4fec213e51551672a722607cd27
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-07-17 09:57:02 +0900

    Get input from user and update board

--- ticTacToe.rb
+++ ticTacToe.rb
@@ -4,49 +4,62 @@
  #if winner, ask to restart game
  #if no winner, ask next player to make their move and repeat
 
+class Game
+  def initialize
+    puts "Let's play Tic Tac Toe."
+    @gameboard = Gameboard.new
+    @player = :X
+    play
+  end
+
+  def play
+    puts "\nPlayer #{@player.to_s}, please enter the number of the square you want to take."
+
+    @position = gets.chomp
+    @gameboard.update(@player, @position)
+    if @gameboard.winner(@player)
+      puts "#{@player} wins!"
+      return
+    elsif @player == :X
+      @player = :O
+      play
+    else
+      @player = :X
+      play
+    end
+  end
+end
+
 class Gameboard
   def initialize
-    @allPositions = Array(1..9)
+    @all_positions = Array(1..9)
 
     display
 
-    @allPositions = Array.new(9, " ")
+    @all_positions = Array.new(9, " ")
     @moves = {X: [], O: []}
   end
 
   def update(player, position)
     player = player.upcase.to_sym
     @moves[player].push(position)
-    @moves[player].each{ |pos| @allPositions[pos - 1] = player.to_s}
+    @moves[player].each{ |pos| @all_positions[pos.to_i   - 1] = player.to_s}
 
     display
+  end
+
+  def winner(player)
   end
 
   private
 
   def display
-    puts "#{@allPositions[0]} | #{@allPositions[1]} | #{@allPositions[2]}"
+    puts "\n#{@all_positions[0]} | #{@all_positions[1]} | #{@all_positions[2]}"
     puts "- | - | -"
-    puts "#{@allPositions[3]} | #{@allPositions[4]} | #{@allPositions[5]}"
+    puts "#{@all_positions[3]} | #{@all_positions[4]} | #{@all_positions[5]}"
     puts "- | - | -"
-    puts "#{@allPositions[6]} | #{@allPositions[7]} | #{@allPositions[8]}"
+    puts "#{@all_positions[6]} | #{@all_positions[7]} | #{@all_positions[8]}"
   end
 end
 
-class Player
-  def initialize
-  end
-
-end
-
-test = Gameboard.new
-test
-puts " "
-test.update("X", 5)
-test
-puts ""
-test.update("o", 1)
-test
-puts ""
-test.update("x", 6)
-test
+Game.new
