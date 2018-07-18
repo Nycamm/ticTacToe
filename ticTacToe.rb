67ad34cd98b31188947ae1f20baae2d626dac5e4
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-07-18 09:54:28 +0900

    add winner? method to check for winner. Check if position is free.

--- ticTacToe.rb
+++ ticTacToe.rb
@@ -7,8 +7,10 @@
 class Game
   def initialize
     puts "Let's play Tic Tac Toe."
+
     @gameboard = Gameboard.new
     @player = :X
+
     play
   end
 
@@ -16,15 +18,20 @@ class Game
     puts "\nPlayer #{@player.to_s}, please enter the number of the square you want to take."
 
     @position = gets.chomp
-    @gameboard.update(@player, @position)
-    if @gameboard.winner(@player)
-      puts "#{@player} wins!"
-      return
+    @gameboard.update(@player, @position.to_i)
+
+    if @gameboard.winner?(@player)
+      puts "\n#{@player} wins!"
+      puts "Would you like to play again? (y/n)"
+
+      gets.chomp.downcase == "y" ? Game.new : return
     elsif @player == :X
       @player = :O
+
       play
     else
       @player = :X
+
       play
     end
   end
@@ -42,23 +49,34 @@ class Gameboard
 
   def update(player, position)
     player = player.upcase.to_sym
-    @moves[player].push(position)
-    @moves[player].each{ |pos| @all_positions[pos.to_i   - 1] = player.to_s}
+
+    if @all_positions[position -1] == " "
+      @moves[player].push(position)
+      @moves[player].each{ |pos| @all_positions[pos.to_i   - 1] = player.to_s}
+    else
+      puts "You can't play there, please choose again"
+      new_position = gets.chomp.to_i
+      update(player, new_position)
+    end
 
     display
   end
 
-  def winner(player)
+  def winner?(player)
+    @winners = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    win = false
+    @winners.each { |w| win = true if (w - @moves[player.upcase.to_sym]).empty?}
+    return win
   end
 
   private
 
   def display
-    puts "\n#{@all_positions[0]} | #{@all_positions[1]} | #{@all_positions[2]}"
+    puts "\n#{@all_positions[6]} | #{@all_positions[7]} | #{@all_positions[8]}"
     puts "- | - | -"
     puts "#{@all_positions[3]} | #{@all_positions[4]} | #{@all_positions[5]}"
     puts "- | - | -"
-    puts "#{@all_positions[6]} | #{@all_positions[7]} | #{@all_positions[8]}"
+    puts "#{@all_positions[0]} | #{@all_positions[1]} | #{@all_positions[2]}"
   end
 end
 
